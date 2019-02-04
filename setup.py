1deb2bc30a36eb5b9547ed589b03a08aed6941ca
Open Close <openandclose23@gmail.com>
2019-02-05 02:25:59 +0900

    Make Add Python3.7

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Utilities',
     ],
     keywords='ini config configparser',
